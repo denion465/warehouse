ab2484347b890d4b30212cde4c4c1bbd25b0ef8c
Daniel Vidal <danielsv465@hotmail.com>
2023-05-12 17:43:11 -0300

    Connect product-microservice to RabbitMQ

--- product-microservice-api/src/main.ts
+++ product-microservice-api/src/main.ts
@@ -1,10 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const {
+    RABBITMQ_USER,
+    RABBITMQ_PASSWORD,
+    RABBITMQ_HOST,
+    RABBITMQ_QUEUE_NAME,
+  } = process.env;
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RABBITMQ_HOST}`],
+      noAck: false,
+      queue: RABBITMQ_QUEUE_NAME,
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen();
+  new Logger('Main').log('Microservice is listening');
 }
 bootstrap();
